b5612d97130bcd74cf30187040bfd0c656b9b3f6
Lauren Pittenger <laurenpittenger@Laurens-MacBook-Pro.local>
2014-05-28 19:22:52 -0400

    first commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+<br class="clear" />
+<div id="upper-footer">
+	<p>While Dr. Robert Brody is training to become a naturopathic physician, he does not maintain a medical license.  Please ensure that you understand the information on this site is provided for informational purposes only and is not meant to substitute for the advice provided by your physician. You should not use the information contained in this website for diagnosing or treating a disease, or using any medications discussed. If you have or suspect a medical condition please seek out a licensed medical professional.</p>
+	</div>
+
+<div id="footer">
+	<p>Website by <a href="http://laurenpittenger.com">Lauren Pittenger<a></p>
+</div>
+<?php wp_footer(); ?>	
+<?php get_template_part('includes/scripts'); ?>
+
+</body>
+</html>
